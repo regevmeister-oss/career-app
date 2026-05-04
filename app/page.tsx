a377c9619334fcc2789bee4c8f7740dca0119257
Roy Regev <regevmeister@gmail.com>
2026-05-04 21:30:24 +0300

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 export default function Home() {
-  const text = Feeling lost? Are you working in a place where you're not truly happy?
+  const text = \Feeling lost? Are you working in a place where you're not truly happy?
 
 If you'd like, we have a short journey for you — a deep dive into your real desires and needs, guiding you toward your true professional path.
 
-Do you want to begin?;
+Do you want to begin?\;
 
   return (
     <main className="min-h-screen flex items-center justify-center text-white text-center p-10">
